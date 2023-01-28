3a4d2edd086f278f73e9d7ec64c8707c55d55bb6
Luba Kassurova <kassurova@icloud.com>
2023-01-27 19:18:38 -0500

    commit tree 6f67dcf5fdad9339687482f22ab48de82dae6b9e

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr.Chocolate", 3, "M", "brown", "sweet", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 2, "M", "purple", "angry", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 6, "F", "yellow", "spicy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Sergeant Brown", 7, "M", "green", "calm", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 4, "F", "pink", "sweet", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 3, "M", "green", "angry", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 2, "F", "light_pink", "silly", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 8, "M", "brown", "angry", 0);
